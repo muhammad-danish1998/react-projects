021e92d6c4bfb65c3ace1005aec49cddcffcdb8e
muhammad Danish <54522496+muhammad-danish1998@users.noreply.github.com>
2024-11-15 15:29:49 +0500

    switch

---
+++ my-project/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+import ToggleBtn from "./components/ToggleBtn";
+const App = () => {
+  return (
+    <div className="flex justify-center items-center">
+   <ToggleBtn />
+    </div>
+  );
+};
+
+export default App;
